bbb7a4b2b7761f9497e7b7486b35e1d8fba9ee99
theo-nidz <theofradin@outlook.com>
2022-10-26 16:39:18 +0200

    maj theo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,12 @@
 
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { LeafletModule } from '@asymmetrik/ngx-leaflet';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LayoutMenuModule } from './layouts/layout-menu/layout-menu.module';
 import { LayoutNomenuModule } from './layouts/layout-nomenu/layout-nomenu.module';
-import { TosComponent } from './pages/tos/tos.component';
+
 
 
 
@@ -17,6 +18,7 @@ import { TosComponent } from './pages/tos/tos.component';
     AppRoutingModule,
     LayoutMenuModule,
     LayoutNomenuModule,
+    LeafletModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/pages/index/index.component.ts
+++ src/app/pages/index/index.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as L from 'leaflet';
 
 @Component({
   selector: 'app-index',
@@ -9,7 +10,19 @@ export class IndexComponent implements OnInit {
 
   constructor() { }
 
+  options = {
+    layers: [
+      L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 18, attribution: '...' }),
+      L.marker([ 49.46223279984973, 1.134840899469156 ])
+    ],
+    zoom: 20,
+    center: L.latLng( 49.46223279984973,1.134840899469156)
+  };
+
+
   ngOnInit(): void {
+
   }
+
 
 }
