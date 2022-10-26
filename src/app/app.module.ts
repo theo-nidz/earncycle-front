bf1e80a21ba63c40e932d1630c547d926d72416b
Marcus MENDY <marcus.mendy27@gmail.com>
2022-10-26 16:34:38 +0200

    Component infos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,13 @@ import { AppComponent } from './app.component';
 import { LayoutMenuModule } from './layouts/layout-menu/layout-menu.module';
 import { LayoutNomenuModule } from './layouts/layout-nomenu/layout-nomenu.module';
 import { TosComponent } from './pages/tos/tos.component';
-import { CouponComponent } from './coupon/coupon.component';
+import { InfosComponent } from './shared/components/infos/infos.component';
 
 
 
 
 @NgModule({
-  declarations: [AppComponent, CouponComponent],
+  declarations: [AppComponent, InfosComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
