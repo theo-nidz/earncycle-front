9e67ce19545f55c5ef4ee53b3b7a8e520c8d82e3
theo-nidz <theofradin@outlook.com>
2022-10-27 17:07:19 +0200

    Ajout Voucher front + TOS

--- src/app/pages/index/index.component.ts
+++ src/app/pages/index/index.component.ts
@@ -4,33 +4,61 @@ import * as L from 'leaflet';
 @Component({
   selector: 'app-index',
   templateUrl: './index.component.html',
-  styleUrls: ['./index.component.css']
+  styleUrls: ['./index.component.css'],
+
 })
 export class IndexComponent implements OnInit {
+  loading = null
 
-  constructor() { }
+  constructor() {}
 
-  myIcon = L.icon({
+  blueBin = L.icon({
     iconUrl: 'assets/bin.svg',
-    iconSize: [38, 95],
-    iconAnchor: [22, 94],
-    popupAnchor: [-3, -76],
+    iconSize: [30, 30],
+    iconAnchor: [20, 25],
   })
+  userMarker = L.icon({
+    iconUrl: 'assets/userMarker.png',
+    iconSize: [30, 30],
+    iconAnchor: [20, 25],
 
-  options = {
-    layers: [
-      
-      L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 18, attribution: '...' }),
-      L.marker([ 49.46223279984973, 1.134840899469156 ],{icon:this.myIcon})
-    ],
-    zoom: 20,
-    center: L.latLng( 49.46223279984973,1.134840899469156),
-  };
+  })
+  //GEOLOCATION
+  lat  = 0
+  long = 0
+  trackMe() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        this.lat= position.coords.latitude
+        this.long = position.coords.longitude
+        console.log('lat',position.coords.latitude)
+        console.log('long',position.coords.longitude)
 
-
-  ngOnInit(): void {
-
+      });
+    } else {
+      alert("Geolocation is not supported by this browser.");
+    }
   }
+  //MAP LEAFLET
+  center = L.latLng(49.4501888, 1.0878976 )
+  zoom = 2
+  options = {
+    zoomControl:false,
+    layers: [
+      L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 18, attribution: '...' })
+    ],
+  };
+  layers = [L.marker([ 49.4501888, 1.0878976 ],{icon:this.userMarker}),L.marker([ 49.4501888, 1.0878976 ],{icon:this.blueBin})]
+  ngOnInit( ): void {
+    this.trackMe()
+  }
+  ngViewInit(): void {
 
-
+    //FOREACH POUR MARKERS
+    //(MARKERS EN BDD).forEach((hit:object) => {
+      // @ts-ignore
+     // markers.push(L.marker([ lat, long ]))
+    //})
+    this.layers = [L.marker([ this.lat, this.long ])]
+  }
 }
