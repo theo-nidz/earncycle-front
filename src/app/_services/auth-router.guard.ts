af9bf7a8dbfdac2ede605f414059624f409bcf4c
Johann six <six.johann@gmail.com>
2022-12-06 16:14:09 +0100

    🎨 improve login

--- src/app/_services/auth-router.guard.ts
+++ src/app/_services/auth-router.guard.ts
@@ -13,11 +13,12 @@ export class AuthRouterGuard implements CanActivate {
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
       // TODO To delete
     console.log('authGuard#canActivate called');
-    if (AppComponent.isLoggedIn && AppComponent.isAdmin) {
+    console.info('AppComponent.getIsLoggedIn(): ' + AppComponent.getIsLoggedIn());
+    if (AppComponent.getIsLoggedIn() && AppComponent.getIsAdmin()) {
       // Add link to admin page
       return true;
     }
-    if (AppComponent.isLoggedIn && AppComponent.isUser) {
+    if (AppComponent.getIsLoggedIn() && AppComponent.getIsUser()) {
       return true;
     }
     return this.router.parseUrl('/login');
